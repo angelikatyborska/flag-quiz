2ca48b162739604b190ba60f454e20e6097cc001
Angelika Cathor <angelikacathor@fastmail.com>
2025-12-07 12:26:33 +0100

    Split inputs by continent

--- bin/generate_flags.exs
+++ bin/generate_flags.exs
@@ -1,84 +1,101 @@
-# TODO: autoread country list
-countries = ["ao", "au", "cl", "pa", "cm", "mn", "ga"]
-
+input_dir = "./lib/flag_quiz/input"
 output_dir = "./output"
 
 File.rm_rf(output_dir)
 File.mkdir(output_dir)
 
-Enum.each(countries, fn code ->
-  File.mkdir(Path.join(output_dir, "/#{code}"))
+continent_dirs = Path.wildcard("#{input_dir}/*")
 
-  flag_string = File.read!("./lib/flag_quiz/input/#{code}.svg")
-  {:ok, flag} = FlagQuiz.Svg.parse_string(flag_string)
-  module = "Elixir.FlagQuiz.Input.#{String.upcase(code)}"
-  available_modifications = apply(String.to_existing_atom(module), :modifications, [])
-  modification_conflicts = apply(String.to_existing_atom(module), :modification_conflicts, [])
+Enum.each(continent_dirs, fn continent_dir ->
+  continent = String.split(continent_dir, "/", trim: true) |> List.last()
+  File.mkdir(Path.join(output_dir, "/#{continent}"))
 
-  modification_combinations =
-    FlagQuiz.Flag.Modification.generate_combinations(
-      available_modifications,
-      modification_conflicts
-    )
+  country_dirs = Path.wildcard("#{continent_dir}/*.ex")
 
-  versions =
-    [%FlagQuiz.Flag.Version{modifications: []}] ++
-      Enum.map(modification_combinations, fn modifications ->
-        %FlagQuiz.Flag.Version{modifications: modifications}
-      end)
+  Enum.each(country_dirs, fn country_dir ->
+    code =
+      String.split(country_dir, "/", trim: true)
+      |> List.last()
+      |> String.split(".")
+      |> List.first()
 
-  data =
-    versions
-    |> Enum.map(fn version ->
-      modified_flag =
-        Enum.reduce(version.modifications, flag, fn modification, acc ->
-          FlagQuiz.Flag.Tweak.apply_tweaks(acc, modification)
-        end)
+    country_output_dir = Path.join(output_dir, "/#{continent}/#{code}")
+    File.mkdir(country_output_dir)
 
-      modifications = Enum.map(version.modifications, & &1.id)
+    flag_string = File.read!("#{input_dir}/#{continent}/#{code}.svg")
+    {:ok, flag} = FlagQuiz.Svg.parse_string(flag_string)
+    module = "Elixir.FlagQuiz.Input.#{String.upcase(code)}"
+    available_modifications = apply(String.to_existing_atom(module), :modifications, [])
+    modification_conflicts = apply(String.to_existing_atom(module), :modification_conflicts, [])
 
-      id = :crypto.hash(:md5, "#{code}.#{Enum.join(modifications, ",")}") |> Base.encode16(case: :lower)
-      filename = "#{code}.#{id}.svg"
-
-      File.write!(
-        Path.join(output_dir, "/#{code}/#{filename}"),
-        FlagQuiz.Svg.export_string(modified_flag)
+    modification_combinations =
+      FlagQuiz.Flag.Modification.generate_combinations(
+        available_modifications,
+        modification_conflicts
       )
 
-      %{id: id, modifications: modifications, filename: filename}
-    end)
+    versions =
+      [%FlagQuiz.Flag.Version{modifications: []}] ++
+        Enum.map(modification_combinations, fn modifications ->
+          %FlagQuiz.Flag.Version{modifications: modifications}
+        end)
 
-  File.write!(Path.join(output_dir, "/#{code}/index.json"), Jason.encode!(data))
+    data =
+      versions
+      |> Enum.map(fn version ->
+        modified_flag =
+          Enum.reduce(version.modifications, flag, fn modification, acc ->
+            FlagQuiz.Flag.Tweak.apply_tweaks(acc, modification)
+          end)
 
-  imgs =
-    data
-    |> Enum.map(fn flag -> "<img src=\"./#{flag.filename}\" />" end)
+        modifications = Enum.map(version.modifications, & &1.id)
 
-  html = """
-  <!DOCTYPE html>
-  <html>
-  <head>
-  <title>#{apply(String.to_existing_atom(module), :name, [])}</title>
-  <style>
-    html {
-      margin: 0;
-      padding: 0;
-      background-color: lightgray;
-    }
+        id =
+          :crypto.hash(:md5, "#{code}.#{Enum.join(modifications, ",")}")
+          |> Base.encode16(case: :lower)
 
-    img {
-      max-width: 100%;
-      height: auto;
-    }
-  </style>
-  </head>
-  <body>
-  <div style="display: grid; grid-template-columns: repeat(8, 1fr); align-items: center; gap: 10px; padding: 10px;">
-  #{imgs}
-  </div>
-  </body>
-  </html>
-  """
+        filename = "#{code}.#{id}.svg"
 
-  File.write(Path.join(output_dir, "/#{code}/index.html"), html)
+        File.write!(
+          Path.join(country_output_dir, filename),
+          FlagQuiz.Svg.export_string(modified_flag)
+        )
+
+        %{id: id, modifications: modifications, filename: filename}
+      end)
+
+    File.write!(Path.join(country_output_dir, "index.json"), Jason.encode!(data))
+
+    imgs =
+      data
+      |> Enum.map(fn flag -> "<img src=\"./#{flag.filename}\" />" end)
+
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>#{apply(String.to_existing_atom(module), :name, [])}</title>
+    <style>
+      html {
+        margin: 0;
+        padding: 0;
+        background-color: lightgray;
+      }
+
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+    </style>
+    </head>
+    <body>
+    <div style="display: grid; grid-template-columns: repeat(8, 1fr); align-items: center; gap: 10px; padding: 10px;">
+    #{imgs}
+    </div>
+    </body>
+    </html>
+    """
+
+    File.write(Path.join(country_output_dir, "index.html"), html)
+  end)
 end)
