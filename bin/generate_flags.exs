9bf52e7918da293196e75483703b97240375ae73
Angelika Cathor <angelikacathor@fastmail.com>
2025-12-07 09:12:44 +0100

    Save JSON data; use random ids

--- bin/generate_flags.exs
+++ bin/generate_flags.exs
@@ -7,35 +7,52 @@ File.rm_rf(output_dir)
 File.mkdir(output_dir)
 
 Enum.each(countries, fn code ->
-   File.mkdir(Path.join(output_dir, "/#{code}"))
+  File.mkdir(Path.join(output_dir, "/#{code}"))
 
   flag_string = File.read!("./lib/flag_quiz/input/#{code}.svg")
   {:ok, flag} = FlagQuiz.Svg.parse_string(flag_string)
   module = "Elixir.FlagQuiz.Input.#{String.upcase(code)}"
   available_modifications = apply(String.to_existing_atom(module), :modifications, [])
   modification_conflicts = apply(String.to_existing_atom(module), :modification_conflicts, [])
-  modification_combinations = FlagQuiz.Flag.Modification.generate_combinations(available_modifications, modification_conflicts)
 
-  versions = [%FlagQuiz.Flag.Version{modifications: []}] ++ Enum.map(modification_combinations, fn modifications -> %FlagQuiz.Flag.Version{modifications: modifications} end)
+  modification_combinations =
+    FlagQuiz.Flag.Modification.generate_combinations(
+      available_modifications,
+      modification_conflicts
+    )
 
-  versions
-  |> Enum.with_index()
-  |> Enum.each(fn {version, index} ->
-
-    modified_flag =
-      Enum.reduce(version.modifications, flag, fn modification, acc ->
-        FlagQuiz.Flag.Tweak.apply_tweaks(acc, modification)
+  versions =
+    [%FlagQuiz.Flag.Version{modifications: []}] ++
+      Enum.map(modification_combinations, fn modifications ->
+        %FlagQuiz.Flag.Version{modifications: modifications}
       end)
 
+  data =
+    versions
+    |> Enum.map(fn version ->
+      modified_flag =
+        Enum.reduce(version.modifications, flag, fn modification, acc ->
+          FlagQuiz.Flag.Tweak.apply_tweaks(acc, modification)
+        end)
+
+      id = for _ <- 1..6, into: "", do: <<Enum.random(?a..?z)>>
+
       # TODO: put input and output in dir per country
-    File.write!(Path.join(output_dir, "/#{code}/#{index}.svg"), FlagQuiz.Svg.export_string(modified_flag))
-  end)
+      File.write!(
+        Path.join(output_dir, "/#{code}/#{id}.svg"),
+        FlagQuiz.Svg.export_string(modified_flag)
+      )
+
+      modifications = Enum.map(version.modifications, & &1.id)
+      %{id: id, modifications: modifications}
+    end)
+
+  File.write!(Path.join(output_dir, "/#{code}/index.json"), Jason.encode!(data))
 
   imgs =
-    versions
-     |> Enum.with_index()
-#    |> Enum.shuffle()
-    |> Enum.map(fn {_, index} -> "<img src=\"./#{index}.svg\" />" end)
+    data
+    |> Enum.map(fn flag -> "<img src=\"./#{flag.id}.svg\" />" end)
+
   html = """
   <!DOCTYPE html>
   <html>
@@ -61,6 +78,6 @@ Enum.each(countries, fn code ->
   </body>
   </html>
   """
-  File.write(Path.join(output_dir, "/#{code}/index.html"), html)
 
+  File.write(Path.join(output_dir, "/#{code}/index.html"), html)
 end)
--- mix.exs
+++ mix.exs
@@ -20,6 +20,6 @@ defmodule FlagQuiz.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:jason, "~> 1.4"}]
   end
 end
